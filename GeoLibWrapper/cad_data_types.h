27236713366bf7f307ec48d2bf01fd4f2eb46b66
tsuyokuro <tsuyokuro.ms@gmai.com>
2022-07-22 19:24:23 +0900

    Modify header for using CadDataTypes

--- GeoLibWrapper/cad_data_types.h
+++ GeoLibWrapper/cad_data_types.h
@@ -1,14 +1,14 @@
 #pragma once
 #ifdef WIN32
 	#ifdef _DEBUG
-		#using "..\\CadDataTypes\\bin\\x86\\Debug\\netcoreapp3.1\\CadDataTypes.dll"
+		#using "..\\CadDataTypes\\bin\\x86\\Debug\\net6.0\\CadDataTypes.dll"
 	#else
-		#using "..\\CadDataTypes\\bin\\x86\\Release\\netcoreapp3.1\\CadDataTypes.dll"
+		#using "..\\CadDataTypes\\bin\\x86\\Release\\net6.0\\CadDataTypes.dll"
 	#endif
 #else
 	#ifdef _DEBUG
-		#using "..\\CadDataTypes\\bin\\x64\\Debug\\netcoreapp3.1\\CadDataTypes.dll"
+		#using "..\\CadDataTypes\\bin\\x64\\Debug\\net6.0\\CadDataTypes.dll"
 	#else
-		#using "..\\CadDataTypes\\bin\\x64\\Release\\netcoreapp3.1\\CadDataTypes.dll"
+		#using "..\\CadDataTypes\\bin\\x64\\Release\\net6.0\\CadDataTypes.dll"
 	#endif
 #endif
