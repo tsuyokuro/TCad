49266cbfc03a53f585a64293b6fd59cbcdf79cd6
tsuyokuro <tsuyokuro.ms@gmai.com>
2022-07-23 16:44:10 +0900

    Remove settings in resource

---
+++ TCad/Properties/TrExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace TCad.MarkupExtensions
+{
+    [MarkupExtensionReturnTypeAttribute(typeof(string))]
+    public class TrExtension : MarkupExtension
+    {
+
+        string _key;
+
+        public TrExtension(string key)
+        {
+            _key = key;
+        }
+
+        const string NotFoundError = "#StringNotFound#";
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (string.IsNullOrEmpty(_key))
+                return NotFoundError;
+
+            return Properties.Resources.ResourceManager.GetString(_key) ?? NotFoundError;
+        }
+    }
+}
