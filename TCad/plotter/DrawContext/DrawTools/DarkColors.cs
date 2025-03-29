4420dad025580bda8645232aeff856793c44a02d
tsuyokuro <tsuyokuro.ms@gmail.com>
2025-03-30 07:33:32 +0900

    Modify DrawTools color setteings

--- TCad/plotter/DrawContext/DrawTools/DarkColors.cs
+++ TCad/plotter/DrawContext/DrawTools/DarkColors.cs
@@ -12,36 +12,35 @@ public class DarkColors : ColorSet
 
     private DarkColors()
     {
-        PenColorTbl[DrawTools.PEN_DEFAULT] = Color.White;
+        PenColorTbl[DrawTools.PEN_DEFAULT] = Color.FromArgb(255, 255, 255);
         PenColorTbl[DrawTools.PEN_SELECTED_POINT] = Color.FromArgb(128, 255, 0);
         PenColorTbl[DrawTools.PEN_CROSS_CURSOR] = Color.FromArgb(32, 64, 64);
         PenColorTbl[DrawTools.PEN_CROSS_CURSOR2] = Color.FromArgb(48, 128, 96);
-        PenColorTbl[DrawTools.PEN_DEFAULT_FIGURE] = Color.White;
-        PenColorTbl[DrawTools.PEN_TEMP_FIGURE] = Color.CadetBlue;
-        PenColorTbl[DrawTools.PEN_POINT_HIGHLIGHT] = Color.Orange;
-        PenColorTbl[DrawTools.PEN_MATCH_SEG] = Color.YellowGreen;
-        PenColorTbl[DrawTools.PEN_LAST_POINT_MARKER] = Color.CornflowerBlue;
-        PenColorTbl[DrawTools.PEN_LAST_POINT_MARKER2] = Color.YellowGreen;
+        PenColorTbl[DrawTools.PEN_DEFAULT_FIGURE] = Color.FromArgb(255, 255, 255);
+        PenColorTbl[DrawTools.PEN_TEMP_FIGURE] = Color.FromArgb(95, 158, 160);
+        PenColorTbl[DrawTools.PEN_POINT_HIGHLIGHT] = Color.FromArgb(255, 165, 0);
+        PenColorTbl[DrawTools.PEN_MATCH_SEG] = Color.FromArgb(154, 205, 50);
+        PenColorTbl[DrawTools.PEN_LAST_POINT_MARKER] = Color.FromArgb(100, 149, 237);
+        PenColorTbl[DrawTools.PEN_LAST_POINT_MARKER2] = Color.FromArgb(154, 205, 50);
         PenColorTbl[DrawTools.PEN_AXIS] = Color.FromArgb(60, 60, 92);
         PenColorTbl[DrawTools.PEN_PAGE_FRAME] = Color.FromArgb(92, 92, 92);
-        PenColorTbl[DrawTools.PEN_TEST_FIGURE] = Color.Yellow;
+        PenColorTbl[DrawTools.PEN_TEST_FIGURE] = Color.FromArgb(255, 255, 0);
         PenColorTbl[DrawTools.PEN_GRID] = Color.FromArgb(92, 92, 92);
         PenColorTbl[DrawTools.PEN_POINT_HIGHLIGHT2] = Color.FromArgb(64, 255, 255);
         PenColorTbl[DrawTools.PEN_FIGURE_HIGHLIGHT] = Color.FromArgb(255, 92, 192);
-        PenColorTbl[DrawTools.PEN_PALE_FIGURE] = Color.FromArgb(0x7E, 0x7E, 0x7E);
-        PenColorTbl[DrawTools.PEN_MEASURE_FIGURE] = Color.OrangeRed;
-        PenColorTbl[DrawTools.PEN_DIMENTION] = Color.FromArgb(0xFF, 128, 192, 255);
+        PenColorTbl[DrawTools.PEN_PALE_FIGURE] = Color.FromArgb(126, 126, 126);
+        PenColorTbl[DrawTools.PEN_MEASURE_FIGURE] = Color.FromArgb(255, 69, 0);
+        PenColorTbl[DrawTools.PEN_DIMENTION] = Color.FromArgb(255, 128, 192, 255);
 
-        PenColorTbl[DrawTools.PEN_MESH_LINE] = Color.FromArgb(0xFF, 0x70, 0x70, 0x70);
-        //PenColorTbl[DrawTools.PEN_MESH_EDGE_LINE] = Color.FromArgb(0xFF, 255, 142, 0); // Orange
-        PenColorTbl[DrawTools.PEN_MESH_EDGE_LINE] = Color.FromArgb(0xFF, 67, 184, 255); // Light Blue
+        PenColorTbl[DrawTools.PEN_MESH_LINE] = Color.FromArgb(112, 112, 112);
+        PenColorTbl[DrawTools.PEN_MESH_EDGE_LINE] = Color.FromArgb(67, 184, 255);
 
-        PenColorTbl[DrawTools.PEN_TEST] = Color.FromArgb(0xFF, 0xBB, 0xCC, 0xDD);
-        PenColorTbl[DrawTools.PEN_NURBS_CTRL_LINE] = Color.FromArgb(0xFF, 0x60, 0xC0, 0x60);
-        PenColorTbl[DrawTools.PEN_DRAG_LINE] = Color.FromArgb(0xFF, 0x60, 0x60, 0x80);
-        PenColorTbl[DrawTools.PEN_NORMAL] = Color.FromArgb(0xFF, 0x00, 0xff, 0x7f);
-        PenColorTbl[DrawTools.PEN_EXT_SNAP] = Color.FromArgb(0xFF, 0xff, 0x00, 0x00);
-        PenColorTbl[DrawTools.PEN_HANDLE_LINE] = Color.YellowGreen;
+        PenColorTbl[DrawTools.PEN_TEST] = Color.FromArgb(187, 204, 221);
+        PenColorTbl[DrawTools.PEN_NURBS_CTRL_LINE] = Color.FromArgb(255, 96, 192, 96);
+        PenColorTbl[DrawTools.PEN_DRAG_LINE] = Color.FromArgb(255, 96, 96, 128);
+        PenColorTbl[DrawTools.PEN_NORMAL] = Color.FromArgb(255, 0, 255, 127);
+        PenColorTbl[DrawTools.PEN_EXT_SNAP] = Color.FromArgb(255, 255, 0, 0);
+        PenColorTbl[DrawTools.PEN_HANDLE_LINE] = Color.FromArgb(154, 205, 50);
         PenColorTbl[DrawTools.PEN_AXIS_X] = Color.FromArgb(112, 50, 50);
         PenColorTbl[DrawTools.PEN_AXIS_Y] = Color.FromArgb(50, 92, 50);
         PenColorTbl[DrawTools.PEN_AXIS_Z] = Color.FromArgb(50, 50, 128);
@@ -54,15 +53,15 @@ public class DarkColors : ColorSet
 
 
         BrushColorTbl[DrawTools.BRUSH_DEFAULT] = Color.FromArgb(255, 255, 255);
-        BrushColorTbl[DrawTools.BRUSH_BACKGROUND] = Color.FromArgb(0x8, 0x8, 0x8);
-        BrushColorTbl[DrawTools.BRUSH_TEXT] = Color.White;
+        BrushColorTbl[DrawTools.BRUSH_BACKGROUND] = Color.FromArgb(8, 8, 8);
+        BrushColorTbl[DrawTools.BRUSH_TEXT] = Color.FromArgb(255, 255, 255);
         BrushColorTbl[DrawTools.BRUSH_DEFAULT_MESH_FILL] = Color.FromArgb(128, 128, 128);
         BrushColorTbl[DrawTools.BRUSH_TRANSPARENT] = Color.FromArgb(0, 0, 0, 0);
-        BrushColorTbl[DrawTools.BRUSH_PALE_TEXT] = Color.FromArgb(0x7E, 0x7E, 0x7E);
+        BrushColorTbl[DrawTools.BRUSH_PALE_TEXT] = Color.FromArgb(126, 126, 126);
 
-        Color AxisLabel_X = Color.FromArgb(0x90, 0x90, 0x90);
-        Color AxisLabel_Y = Color.FromArgb(0x90, 0x90, 0x90);
-        Color AxisLabel_Z = Color.FromArgb(0x90, 0x90, 0x90);
+        Color AxisLabel_X = Color.FromArgb(144, 144, 144);
+        Color AxisLabel_Y = Color.FromArgb(144, 144, 144);
+        Color AxisLabel_Z = Color.FromArgb(144, 144, 144);
 
         BrushColorTbl[DrawTools.BRUSH_AXIS_LABEL_X] = AxisLabel_X;
         BrushColorTbl[DrawTools.BRUSH_AXIS_LABEL_Y] = AxisLabel_Y;
--- TCad/plotter/DrawContext/DrawTools/LightColors.cs
+++ TCad/plotter/DrawContext/DrawTools/LightColors.cs
@@ -12,33 +12,33 @@ public class LightColors : ColorSet
 
     private LightColors()
     {
-        PenColorTbl[DrawTools.PEN_DEFAULT] = Color.Black;
+        PenColorTbl[DrawTools.PEN_DEFAULT] = Color.FromArgb(0,0,0);
         PenColorTbl[DrawTools.PEN_SELECTED_POINT] = Color.FromArgb(128, 255, 0);
         PenColorTbl[DrawTools.PEN_CROSS_CURSOR] = Color.FromArgb(64, 192, 192);
         PenColorTbl[DrawTools.PEN_CROSS_CURSOR2] = Color.FromArgb(51, 153, 153);
-        PenColorTbl[DrawTools.PEN_DEFAULT_FIGURE] = Color.Black;
-        PenColorTbl[DrawTools.PEN_TEMP_FIGURE] = Color.CadetBlue;
-        PenColorTbl[DrawTools.PEN_POINT_HIGHLIGHT] = Color.Orange;
-        PenColorTbl[DrawTools.PEN_MATCH_SEG] = Color.Green;
-        PenColorTbl[DrawTools.PEN_LAST_POINT_MARKER] = Color.Crimson;
-        PenColorTbl[DrawTools.PEN_LAST_POINT_MARKER2] = Color.YellowGreen;
+        PenColorTbl[DrawTools.PEN_DEFAULT_FIGURE] = Color.FromArgb(0, 0, 0);
+        PenColorTbl[DrawTools.PEN_TEMP_FIGURE] = Color.FromArgb(95, 158, 160);
+        PenColorTbl[DrawTools.PEN_POINT_HIGHLIGHT] = Color.FromArgb(255, 165, 0);
+        PenColorTbl[DrawTools.PEN_MATCH_SEG] = Color.FromArgb(0, 128, 0);
+        PenColorTbl[DrawTools.PEN_LAST_POINT_MARKER] = Color.FromArgb(220, 20, 60);
+        PenColorTbl[DrawTools.PEN_LAST_POINT_MARKER2] = Color.FromArgb(154, 205, 50);
         PenColorTbl[DrawTools.PEN_AXIS] = Color.FromArgb(60, 60, 92);
         PenColorTbl[DrawTools.PEN_PAGE_FRAME] = Color.FromArgb(92, 92, 92);
-        PenColorTbl[DrawTools.PEN_TEST_FIGURE] = Color.Yellow;
+        PenColorTbl[DrawTools.PEN_TEST_FIGURE] = Color.FromArgb(255, 255, 0);
         PenColorTbl[DrawTools.PEN_GRID] = Color.FromArgb(192, 128, 92);
         PenColorTbl[DrawTools.PEN_POINT_HIGHLIGHT2] = Color.FromArgb(64, 255, 255);
-        PenColorTbl[DrawTools.PEN_FIGURE_HIGHLIGHT] = Color.HotPink;
-        PenColorTbl[DrawTools.PEN_PALE_FIGURE] = Color.FromArgb(0x7E, 0x7E, 0x7E);
-        PenColorTbl[DrawTools.PEN_MEASURE_FIGURE] = Color.OrangeRed;
+        PenColorTbl[DrawTools.PEN_FIGURE_HIGHLIGHT] = Color.FromArgb(255, 105, 180);
+        PenColorTbl[DrawTools.PEN_PALE_FIGURE] = Color.FromArgb(126, 126, 126);
+        PenColorTbl[DrawTools.PEN_MEASURE_FIGURE] = Color.FromArgb(255, 69, 0);
         PenColorTbl[DrawTools.PEN_DIMENTION] = Color.FromArgb(0xFF, 128, 192, 255);
-        PenColorTbl[DrawTools.PEN_MESH_LINE] = Color.FromArgb(0xFF, 0x70, 0x70, 0x70);
-        PenColorTbl[DrawTools.PEN_MESH_EDGE_LINE] = Color.Black;
-        PenColorTbl[DrawTools.PEN_TEST] = Color.FromArgb(0xFF, 0xBB, 0xCC, 0xDD);
-        PenColorTbl[DrawTools.PEN_NURBS_CTRL_LINE] = Color.FromArgb(0xFF, 0x60, 0xC0, 0x60);
-        PenColorTbl[DrawTools.PEN_DRAG_LINE] = Color.FromArgb(0xFF, 0x60, 0x60, 0x80);
-        PenColorTbl[DrawTools.PEN_NORMAL] = Color.FromArgb(0xFF, 0x00, 0xff, 0x7f);
-        PenColorTbl[DrawTools.PEN_EXT_SNAP] = Color.FromArgb(0xFF, 0xff, 0x00, 0x00);
-        PenColorTbl[DrawTools.PEN_HANDLE_LINE] = Color.YellowGreen;
+        PenColorTbl[DrawTools.PEN_MESH_LINE] = Color.FromArgb(0xFF, 112, 112, 112);
+        PenColorTbl[DrawTools.PEN_MESH_EDGE_LINE] = Color.FromArgb(0, 0, 0);
+        PenColorTbl[DrawTools.PEN_TEST] = Color.FromArgb(0xFF, 187, 204, 221);
+        PenColorTbl[DrawTools.PEN_NURBS_CTRL_LINE] = Color.FromArgb(96, 192, 96);
+        PenColorTbl[DrawTools.PEN_DRAG_LINE] = Color.FromArgb(96, 96, 128);
+        PenColorTbl[DrawTools.PEN_NORMAL] = Color.FromArgb(0, 255, 127);
+        PenColorTbl[DrawTools.PEN_EXT_SNAP] = Color.FromArgb(255, 0, 0);
+        PenColorTbl[DrawTools.PEN_HANDLE_LINE] = Color.FromArgb(154, 205, 50);
         PenColorTbl[DrawTools.PEN_AXIS_X] = Color.FromArgb(192, 60, 60);
         PenColorTbl[DrawTools.PEN_AXIS_Y] = Color.FromArgb(60, 128, 60);
         PenColorTbl[DrawTools.PEN_AXIS_Z] = Color.FromArgb(60, 60, 192);
@@ -46,20 +46,20 @@ public class LightColors : ColorSet
         PenColorTbl[DrawTools.PEN_COMPASS_X] = Color.FromArgb(192, 60, 60);
         PenColorTbl[DrawTools.PEN_COMPASS_Y] = Color.FromArgb(60, 128, 60);
         PenColorTbl[DrawTools.PEN_COMPASS_Z] = Color.FromArgb(60, 60, 192);
-        PenColorTbl[DrawTools.PEN_CURRENT_FIG_SELECTED_POINT] = Color.HotPink;
+        PenColorTbl[DrawTools.PEN_CURRENT_FIG_SELECTED_POINT] = Color.FromArgb(255, 105, 180);
         PenColorTbl[DrawTools.PEN_LAST_SEL_SEG] = Color.FromArgb(0, 0, 192);
 
 
         BrushColorTbl[DrawTools.BRUSH_DEFAULT] = Color.FromArgb(128, 128, 128);
         BrushColorTbl[DrawTools.BRUSH_BACKGROUND] = Color.FromArgb(245, 245, 255);
-        BrushColorTbl[DrawTools.BRUSH_TEXT] = Color.Black;
+        BrushColorTbl[DrawTools.BRUSH_TEXT] = Color.FromArgb(0, 0, 0);
         BrushColorTbl[DrawTools.BRUSH_DEFAULT_MESH_FILL] = Color.FromArgb(192, 192, 192);
         BrushColorTbl[DrawTools.BRUSH_TRANSPARENT] = Color.FromArgb(0, 0, 0, 0);
-        BrushColorTbl[DrawTools.BRUSH_PALE_TEXT] = Color.FromArgb(0x7E, 0x7E, 0x7E);
+        BrushColorTbl[DrawTools.BRUSH_PALE_TEXT] = Color.FromArgb(126, 126, 126);
 
-        Color AxisLabel_X = Color.FromArgb(0x30, 0x30, 0x30);
-        Color AxisLabel_Y = Color.FromArgb(0x30, 0x30, 0x30);
-        Color AxisLabel_Z = Color.FromArgb(0x30, 0x30, 0x30);
+        Color AxisLabel_X = Color.FromArgb(48, 48, 48);
+        Color AxisLabel_Y = Color.FromArgb(48, 48, 48);
+        Color AxisLabel_Z = Color.FromArgb(48, 48, 48);
 
         BrushColorTbl[DrawTools.BRUSH_AXIS_LABEL_X] = AxisLabel_X;
         BrushColorTbl[DrawTools.BRUSH_AXIS_LABEL_Y] = AxisLabel_Y;
@@ -69,6 +69,6 @@ public class LightColors : ColorSet
         BrushColorTbl[DrawTools.BRUSH_COMPASS_LABEL_Z] = AxisLabel_Z;
 
         BrushColorTbl[DrawTools.BRUSH_SELECTED_POINT] = Color.FromArgb(128, 255, 0);
-        BrushColorTbl[DrawTools.BRUSH_CURRENT_FIG_SELECTED_POINT] = Color.HotPink;
+        BrushColorTbl[DrawTools.BRUSH_CURRENT_FIG_SELECTED_POINT] = Color.FromArgb(255, 105, 180);
     }
 }
